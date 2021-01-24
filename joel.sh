67c1ba3ccb9ecdec61d53729db6e9f38d7715c6e
Jeff Geerling <geerlingguy@mac.com>
2021-01-24 17:56:07 +0000

    Update joel.sh

--- joel.sh
+++ joel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# That thing above is called a "she-bang" and I havent been able to hear what its about because I'm always giggling
+# That thing above is called a "she-bang" and I havent been able to hear 
+# what its about because I'm always giggling
 echo "This is a shell script, doing things and making life interesting"
 echo "All this does is run a bunch of temrinal commands in order"
 echo "Let's install Homebrew"
@@ -16,7 +17,7 @@ mkdir ~/Library/Mobile Documents/com~apple~CloudDocs/prefs/brewfile
 cowsay "Setting The MacPrefs Directory"
 export MACPREFS_BACKUP_DIR="$HOME/Library/Mobile Documents/com~apple~CloudDocs/prefs/macprefs"
 cowsay -s "Backing up your Prefs"
-macprefs backup
+sudo macprefs backup
 cowsay -s "And taking a big ol' dump(file) in your special directory"
 brew bundle dump --file ~/Library/Mobile Documents/com~apple~CloudDocs/prefs/brew/Brewfile
 cowsay -s "And now we shall clone the Git repository of this, so you can learns!!"
